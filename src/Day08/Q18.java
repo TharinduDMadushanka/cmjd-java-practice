25be0d10838abeef77b3d2566c46a4ee5b660c54
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-04-20 12:57:50 +0530

    first commit

---
+++ src/Day08/Q18.java
@@ -0,0 +1,15 @@
+package Day08;
+
+class Q18 {
+    public static void main(String[] args) {
+        int[] ar = {1,2,4,6,8,3,6,4,5,6,7,2,1} ;
+
+        for(long a : ar){ // Legal
+            System.out.print(a + " ");
+        }
+
+//        for(byte a : ar){  ---->  Illegal (narrow casting)
+//            System.out.print(a + " ");
+//        }
+    }
+}
---
+++ src/Day08/Q19.java
@@ -0,0 +1,7 @@
+package Day08;
+
+public class Q19 {
+    public static void main(String[] args) {
+
+    }
+}
