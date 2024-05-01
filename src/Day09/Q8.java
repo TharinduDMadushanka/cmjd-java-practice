0cec82a4b76a691e1b066e5da922b513636f933e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-01 20:58:25 +0530

    cmjd day09

--- src/Day09/Q8.java
+++ src/Day09/Q8.java
@@ -2,22 +2,16 @@ package Day09;
 
 import java.util.Arrays;
 
-class Q9 {
+class Q8 {
 
     public static void main(String[] args) {
-        int[] ar = {32, 45, 68, 74, 59, 99, 14, 39, 24, 64, 92};
+        int ar[] = {32,45,68,74,59,99,14,39,24,64,92};
         System.out.println(Arrays.toString(ar)); // [32,45,68,74,59,99,14,39,24,64,92]
         reverse(ar);
         System.out.println(Arrays.toString(ar)); // [92,64,24..................45,32]
     }
 
-    public static void reverse(int[] ar) {
-        for (int i = 0, j = ar.length - 1; i < j; i++, j--) {
-            int temp = ar[i];
-            ar[i] = ar[j];
-            ar[j] = temp;
-        }
+    public static void reverse(int[] ar){
+
     }
-
 }
-
