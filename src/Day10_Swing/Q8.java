cd767017222245a036ce3327263686599d2c41d0
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-04 11:31:31 +0530

    cmjd day 10

--- src/Day10_Swing/Q8.java
+++ src/Day10_Swing/Q8.java
@@ -10,7 +10,7 @@ import java.awt.*;
 public class Q8{
     public static void main(String[] args) {
 
-        Calculator3 c1 = new Calculator3(); // there are not class called Calculator then we have to do extend JFrame class and inherit
+        Calculator4 c1 = new Calculator4(); // there are not class called Calculator then we have to do extend JFrame class and inherit
 
     }
 }
@@ -24,40 +24,35 @@ class Calculator4 extends JFrame{ // inherit JFrame class
     JButton button5;
     JLabel label;
 
-    Calculator40(){
+    Calculator4(){
         setSize(300,400);
         setTitle("Calculator");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
 
-        setLayout(new FlowLayout()); // --> default float all buttons into center
-//        setLayout(new FlowLayout(FlowLayout.RIGHT)); --> float all buttons to right
-//        setLayout(new FlowLayout(FlowLayout.LEFT)); --> float all buttons to left
+        setLayout(new GridLayout(2,2));
 
         button1=new JButton();
-        button1.setText("Click");
+        button1.setText("Click1");
         button1.setFont(new Font("",1,25));
         add(button1);
 
         button2=new JButton();
-        button2.setText("Click");
+        button2.setText("Click2");
         button2.setFont(new Font("",1,25));
         add(button2);
 
         button3=new JButton();
-        button3.setText("Click");
+        button3.setText("Click3");
         button3.setFont(new Font("",1,25));
         add(button3);
 
         button4=new JButton();
-        button4.setText("Click");
+        button4.setText("Click4");
         button4.setFont(new Font("",1,25));
         add(button4);
 
-        button5=new JButton();
-        button5.setText("Click");
-        button5.setFont(new Font("",1,25));
-        add(button5);
+
 
         setVisible(true);
     }
