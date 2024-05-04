f36093db07a6f4d5639bd91829ea1fa1c11561fb
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-04 08:23:40 +0530

    cmjd day 10

---
+++ src/Day10_Swing/Q1.java
@@ -0,0 +1,19 @@
+package Day10_Swing;
+
+/**
+ * Create Swing Application
+ */
+
+import javax.swing.*;
+
+public class Q1 {
+    public static void main(String[] args) {
+
+        JFrame f1=new JFrame();
+        f1.setSize(400,300);
+        f1.setTitle("Test Window");
+        f1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        f1.setLocationRelativeTo(null);
+        f1.setVisible(true);
+    }
+}
