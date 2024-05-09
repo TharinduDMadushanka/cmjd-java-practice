20b2aba124d3ce4917f78d17b8725c854cb49cda
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-09 14:39:50 +0530

    first commit

--- src/Day10_Swing/Q13.java
+++ src/Day10_Swing/Q13.java
@@ -13,15 +13,39 @@ public class Q13 {
 
 class AddCustomerForm extends JFrame {
 
-    /**
-     * Step 1 create gui
-     */
+    JLabel titleLable;
+    JButton addButton,cancelButton;
+
     AddCustomerForm(){
+        /**
+         * Step 1 create gui
+         */
+
         setSize(400,400);
         setTitle("Add Customer Form");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setLayout(new BorderLayout());
+
+        /**
+         * Step 2 --> create Title and add&cancel buttons
+         */
+        titleLable=new JLabel("Add Customer form");
+        titleLable.setFont(new Font("",1,32));
+        titleLable.setHorizontalAlignment(JLabel.CENTER);
+        add("North",titleLable);
+
+        JPanel bottomPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
+
+        addButton=new JButton("Add");
+        addButton.setFont(new Font("",1,12));
+        bottomPanel.add(addButton);
+
+        cancelButton=new JButton("Cancel");
+        cancelButton.setFont(new Font("",1,12));
+        bottomPanel.add(cancelButton);
+
+        add("South",bottomPanel);
 
         setVisible(true);
     }
