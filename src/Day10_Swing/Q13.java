b42e836cca0d181c2459bbe182f4168c76227ab6
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-09 14:47:16 +0530

    first commit

--- src/Day10_Swing/Q13.java
+++ src/Day10_Swing/Q13.java
@@ -13,8 +13,9 @@ public class Q13 {
 
 class AddCustomerForm extends JFrame {
 
-    JLabel titleLable;
+    JLabel titleLable,idLabel,nameLabel,addressLabel,nicLabel;
     JButton addButton,cancelButton;
+    JTextField idTextField, nameTextField, addressTextField, nicTextField;
 
     AddCustomerForm(){
         /**
@@ -28,12 +29,58 @@ class AddCustomerForm extends JFrame {
         setLayout(new BorderLayout());
 
         /**
-         * Step 2 --> create Title and add&cancel buttons
+         * Step 2 --> create Title
          */
         titleLable=new JLabel("Add Customer form");
         titleLable.setFont(new Font("",1,32));
         titleLable.setHorizontalAlignment(JLabel.CENTER);
         add("North",titleLable);
+
+
+        /**
+         * Step 3
+         */
+
+        JPanel centerPanel=new JPanel(new GridLayout(4,2));
+        Font font=new Font("",1,15);
+
+        idLabel=new JLabel("Id:");
+        idLabel.setFont(font);
+        centerPanel.add(idLabel);
+
+        idTextField=new JTextField(5);
+        idTextField.setFont(font);
+        centerPanel.add(idTextField);
+
+        nameLabel = new JLabel("Name");
+        nameLabel.setFont(font);
+        centerPanel.add(nameLabel);
+
+        nameTextField = new JTextField(15);
+        nameTextField.setFont(font);
+        centerPanel.add(nameTextField);
+
+        addressLabel = new JLabel("Address");
+        addressLabel.setFont(font);
+        centerPanel.add(addressLabel);
+
+        addressTextField = new JTextField(20);
+        addressTextField.setFont(font);
+        centerPanel.add(addressTextField);
+
+        nicLabel = new JLabel("NIC");
+        nicLabel.setFont(font);
+        centerPanel.add(nicLabel);
+
+        nicTextField = new JTextField(10);
+        nicTextField.setFont(font);
+        centerPanel.add(nicTextField);
+
+        add("Center", centerPanel);
+
+        /**
+         * Step 2 --> add&cancel buttons
+         */
 
         JPanel bottomPanel=new JPanel(new FlowLayout(FlowLayout.RIGHT));
 
@@ -45,8 +92,8 @@ class AddCustomerForm extends JFrame {
         cancelButton.setFont(new Font("",1,12));
         bottomPanel.add(cancelButton);
 
-        add("South",bottomPanel);
 
+        add("South",bottomPanel);
         setVisible(true);
     }
 
