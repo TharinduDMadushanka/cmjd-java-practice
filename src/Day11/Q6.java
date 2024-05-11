397c7fe92b1a781b92fe9aa3e320f5b7da352a51
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-11 12:00:10 +0530

    first commit

--- src/Day11/Q6.java
+++ src/Day11/Q6.java
@@ -1,30 +1,30 @@
-package Day11;
-
-/**
- * Option 2 --. default constructor
- */
-
-public class Q6 {
-    public static void main(String[] args) {
-        A a = new A();
-        B b1 = new B(a);
-        b1.mB();
-    }
-}
-class A{
-    public void mA(){
-        System.out.println("mA of A");
-    }
-}
-
-class B{
-    A a1;//reference --> given error because a1 not initialize
-    B(A a1){
-        this.a1 = a1;
-    }
-
-
-    public void mB(){
-        a1.mA();
-    }
-}
+//package Day11;
+//
+///**
+// * Option 2 --. default constructor
+// */
+//
+//public class Q6 {
+//    public static void main(String[] args) {
+//        A4 a = new A4();
+//        B5 b1 = new B5(a);
+//        b1.mB();
+//    }
+//}
+//class A{
+//    public void mA(){
+//        System.out.println("mA of A");
+//    }
+//}
+//
+//class B{
+//    A4 a1;//reference --> given error because a1 not initialize
+//    B(A4 a1){
+//        this.a1 = a1;
+//    }
+//
+//
+//    public void mB(){
+//        a1.mA();
+//    }
+//}
