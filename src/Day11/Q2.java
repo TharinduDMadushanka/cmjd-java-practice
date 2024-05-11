f5f09ebbe24542c7f729cb86ca67f54c99ce7611
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-11 08:53:18 +0530

    first commit

--- src/Day11/Q2.java
+++ src/Day11/Q2.java
@@ -1,6 +1,8 @@
 package Day11;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -23,14 +25,22 @@ public class Q2 {
             setLocationRelativeTo(null);
             setLayout(new FlowLayout());
 
-            textField=new JTextField(20);
+            textField=new JTextField(20); //text field
             setFont(new Font("",1,20));
             add(textField);
 
-            slider=new JSlider(JSlider.VERTICAL,0,100,50);
+            slider=new JSlider(JSlider.VERTICAL,0,100,50); //slider
             slider.setMajorTickSpacing(10);
             slider.setPaintLabels(true);
             add(slider);
+
+            slider.addChangeListener(new ChangeListener() {
+                @Override
+                public void stateChanged(ChangeEvent e) {
+                    int value= slider.getValue();
+                    textField.setText(value+""); //Integer.toString(value)
+                }
+            });
 
             setVisible(true);
         }
