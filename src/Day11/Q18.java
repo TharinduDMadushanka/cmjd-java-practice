bf568150f9205633f71edaf5ce1efc740f4b4f6d
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-21 20:29:23 +0530

    first commit

--- src/Day11/Q18.java
+++ src/Day11/Q18.java
@@ -4,24 +4,24 @@
  * v1.park(); --> in here compiler chang object vehicle to car.
  */
 
-package Day11;
-
-class Vehicle {
-    public void park() {
-        System.out.println("Vehicle Parking");
-    }
-}
-
-class Car extends Vehicle {
-    public void park() {
-        System.out.println("Car Parking");
-    }
-}
-
-class Q18 {
-    public static void main(String[] args) {
-        Vehicle v1 = new Car();
-        v1.park();
-    }
-}
+//package Day11;
+//
+//class Vehicle {
+//    public void park() {
+//        System.out.println("Vehicle Parking");
+//    }
+//}
+//
+//class Car extends Vehicle {
+//    public void park() {
+//        System.out.println("Car Parking");
+//    }
+//}
+//
+//class Q18 {
+//    public static void main(String[] args) {
+//        Vehicle v1 = new Car();
+//        v1.park();
+//    }
+//}
 
