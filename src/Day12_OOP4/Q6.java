e165b73eb320d12b110ec30ca2df945922cee82e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-22 14:22:37 +0530

    first commit

--- src/Day12_OOP4/Q6.java
+++ src/Day12_OOP4/Q6.java
@@ -4,17 +4,17 @@
 
 package Day12_OOP4;
 
-class Customer /* compiler inserts "extends Object" */{
-
-}
-
-class Q6 {
-    public static void main(String[] args) {
-        Customer c1 = new Customer();
-        c1.hashCode(); // Legal
-        c1.getClass(); // Legal
-        c1.toString(); // Legal
-        c1.equals(c1); // Legal
-    }
-}
+//class Customer /* compiler inserts "extends Object" */{
+//
+//}
+//
+//class Q6 {
+//    public static void main(String[] args) {
+//        Customer c1 = new Customer();
+//        c1.hashCode(); // Legal
+//        c1.getClass(); // Legal
+//        c1.toString(); // Legal
+//        c1.equals(c1); // Legal
+//    }
+//}
 
