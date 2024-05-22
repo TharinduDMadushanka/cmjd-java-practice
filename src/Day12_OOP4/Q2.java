10521ac1c8677415fdadefc5b5f341f23f69857f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-22 13:36:48 +0530

    first commit

--- src/Day12_OOP4/Q2.java
+++ src/Day12_OOP4/Q2.java
@@ -6,22 +6,22 @@ class Vehicle{
     public void park(){
         System.out.println("Vehicle Parking");
     }
-    public void callPark(){
-        park();
-    }
 }
 
 class Car extends Vehicle{
     public void park(){
         System.out.println("Car Parking");
     }
-}
 
-class Q2{
-    public static void main(String[] args) {
-        Car c1 = new Car();
-        c1.callPark();
-
+    public void callPark(){
+        park(); // call from Car
+        super.park(); // call from Vehicle
     }
 }
 
+class Q2 {
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.callPark();
+    }
+}
