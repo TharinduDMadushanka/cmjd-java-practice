4f5c3fcd47de8f0f5b66f9dfaef16c74343084e7
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-22 13:44:14 +0530

    first commit

--- src/Day12_OOP4/Q5.java
+++ src/Day12_OOP4/Q5.java
@@ -1,29 +1,24 @@
 package Day12_OOP4;
 
 //class Vehicle{
-//    int year = 2000;
 //    public void park(){
 //        System.out.println("Vehicle Parking");
+//    }
+//    public void callPark(){
+//        park();
 //    }
 //}
 //
 //class Car extends Vehicle{
-//    int year = 2005;
 //    public void park(){
 //        System.out.println("Car Parking");
 //    }
-//
-//    public void mC(){
-//        int year = 2010;
-//        System.out.println("Local : " + year); // 2010
-//        System.out.println("Car : " + this.year); // 2005
-//        System.out.println("Vehicle : " + super.year); //2000
-//    }
 //}
 //
-//class Q4 {
+//class Q5 {
 //    public static void main(String[] args) {
 //        Car c1 = new Car();
-//        c1.mC();
+//        c1.callPark(); //---> call subclass override park method
+//
 //    }
 //}
