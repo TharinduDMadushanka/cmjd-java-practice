f99ec2c06620433bc8d06fb5a495bbaad2813489
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-22 13:35:52 +0530

    first commit

--- src/Day12_OOP4/Q2.java
+++ src/Day12_OOP4/Q2.java
@@ -1,4 +1,27 @@
+// eX 01 Q554
+
 package Day12_OOP4;
 
-public class Q2 {
+class Vehicle{
+    public void park(){
+        System.out.println("Vehicle Parking");
+    }
+    public void callPark(){
+        park();
+    }
 }
+
+class Car extends Vehicle{
+    public void park(){
+        System.out.println("Car Parking");
+    }
+}
+
+class Q2{
+    public static void main(String[] args) {
+        Car c1 = new Car();
+        c1.callPark();
+
+    }
+}
+
