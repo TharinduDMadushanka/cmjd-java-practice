e8f9350c9da2af70773e5a5427a2976c263b244c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 18:24:53 +0530

    first commit

--- src/Day12_OOP4/Q40.java
+++ src/Day12_OOP4/Q40.java
@@ -5,16 +5,16 @@ package Day12_OOP4;
  *  we can make abs sub cls from non abs super class
  */
 
-class Animal{
-    public void eat(){
-        //
-    }
-}
-
-abstract class Lion extends Animal{
-    abstract public void eat(); // Legal
-}
-
-class Dog extends Animal{} // Legal
-
-class Cat extends Lion{} // Illegal
+//class Animal{
+//    public void eat(){
+//        //
+//    }
+//}
+//
+//abstract class Lion extends Animal{
+//    abstract public void eat(); // Legal
+//}
+//
+//class Dog extends Animal{} // Legal
+//
+//class Cat extends Lion{} // Illegal
---
+++ src/Day12_OOP4/Q41.java
@@ -0,0 +1,7 @@
+/**
+ * Java Interfaces
+ */
+
+package Day12_OOP4;
+public class Q41 {
+}
