66239e5432a56f4a666e850426eaa2484165e4aa
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 10:43:08 +0530

    first commit

--- src/Day12_OOP4/Q36.java
+++ src/Day12_OOP4/Q36.java
@@ -3,8 +3,8 @@ package Day12_OOP4;
  * Case 9
  * cant declare abstract attributes
  */
-abstract class Vehicle{
-    int year;
-    abstract String model; // Illegal
-    abstract public void park();
-}
+//abstract class Vehicle{
+//    int year;
+//    abstract String model; // Illegal
+//    abstract public void park();
+//}
