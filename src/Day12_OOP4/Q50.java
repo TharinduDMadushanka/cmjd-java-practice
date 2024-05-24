6554e1676b467c98de1c8135d888508f3a9ef008
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 21:56:08 +0530

    first commit

--- src/Day12_OOP4/Q50.java
+++ src/Day12_OOP4/Q50.java
@@ -2,19 +2,19 @@ package Day12_OOP4;
 /**
  * Case 08
  */
-interface Vehicle{
-    void park();
-}
-
-class Car implements Vehicle{
-    public void park(){
-        System.out.println("Car parking");
-    }
-}
-
-class Q50 {
-    public static void main(String[] args) {
-        Vehicle v1; // Legal
-        v1 = new Car(); // Legal
-    }
-}
+//interface Vehicle{
+//    void park();
+//}
+//
+//class Car implements Vehicle{
+//    public void park(){
+//        System.out.println("Car parking");
+//    }
+//}
+//
+//class Q50 {
+//    public static void main(String[] args) {
+//        Vehicle v1; // Legal
+//        v1 = new Car(); // Legal
+//    }
+//}
