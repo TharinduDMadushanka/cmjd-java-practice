a79cb2a79af7e859546ba5f8f09a68176db76ea2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 09:00:29 +0530

    first commit

--- src/Day12_OOP4/Q24.java
+++ src/Day12_OOP4/Q24.java
@@ -2,6 +2,15 @@
  * Rule 3 “final”
  */
 
-package Day12_OOP4;
-public class Q24 {
-}
+//package Day12_OOP4;
+//class Vehicle{
+//    public final void park(){
+//        System.out.println("Vehicle Parking");
+//    }
+//}
+//
+//class Car extends Vehicle{
+//    public void park(){  // Illegal --> cant override
+//        System.out.println("Car Parking");
+//    }
+//}
