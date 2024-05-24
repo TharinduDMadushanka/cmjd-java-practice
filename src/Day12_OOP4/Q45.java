68937e635a0c3ac173f474c35bf48b8c06c70562
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 18:39:13 +0530

    first commit

---
+++ src/Day12_OOP4/Q45.java
@@ -0,0 +1,9 @@
+/**
+ * Case 3
+ * interface methods are abstract
+ */
+package Day12_OOP4;
+//interface Vehicle{
+//    public void park(); // Implicitly abstract
+//    abstract public void start(); // Implicitly abstract
+//}
