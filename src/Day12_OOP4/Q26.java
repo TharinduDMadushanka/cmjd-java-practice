f2b712fb676b7ef27b9ae7a471a1a7241d030736
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 10:20:40 +0530

    first commit

--- src/Day12_OOP4/Q26.java
+++ src/Day12_OOP4/Q26.java
@@ -3,5 +3,33 @@
  */
 
 package Day12_OOP4;
-public class Q26 {
+class Vehicle{
+    public void park(); // Illegal--> without body method
 }
+
+class Car extends Vehicle{
+    public void park(){
+        System.out.println("Car Parking");
+    }
+}
+
+class Bus extends Vehicle{
+    public void park(){
+        System.out.println("Bus Parking");
+    }
+}
+
+class Van extends Vehicle{
+    public void park(){
+        System.out.println("Van Parking");
+    }
+}
+class Example {
+    public static void main(String[] args) {
+        Vehicle[] vehicles = {new Car(), new Bus(), new Van()};
+        for (Vehicle vehicle : vehicles) {
+            vehicle.park();
+        }
+    }
+}
+
