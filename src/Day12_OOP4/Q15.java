ffafa0430e190676de1668bc65dc2c516f57c6f4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 08:31:28 +0530

    first commit

---
+++ src/Day12_OOP4/Q15.java
@@ -0,0 +1,24 @@
+package Day12_OOP4;
+
+class Customer{
+    private int code;
+    private String name;
+
+    public Customer(int code, String name){
+        this.code = code;
+        this.name = name;
+    }
+
+    // public String toString(){
+    //     int hashCode = hashCode();
+    //     String hex = Integer.toHexString(hashCode);
+    //     return getClass().getName() + "@" + hex;
+    // }
+}
+
+class Q15 {
+    public static void main(String[] args) {
+        Customer c1 = new Customer(1001, "Customer 1");
+        System.out.println(c1.toString()); // 1001-Customer 1
+    }
+}
