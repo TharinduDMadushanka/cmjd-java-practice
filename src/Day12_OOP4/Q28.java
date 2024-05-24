efa7b12f0bd9a68dc2f12839f7d3be874bdec7ad
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 10:25:16 +0530

    first commit

--- src/Day12_OOP4/Q28.java
+++ src/Day12_OOP4/Q28.java
@@ -1,5 +1,9 @@
 package Day12_OOP4;
 
+/**
+ * Case 1
+ */
+
 //class Vehicle{
 //    public void park(); // Illegal --> cant declare method without body
 //}
---
+++ src/Day12_OOP4/Q29.java
@@ -0,0 +1,5 @@
+package Day12_OOP4;
+
+//class Vehicle{ // Illegal --> abstract method should in the abstract class
+//    abstract public void park();
+//}
