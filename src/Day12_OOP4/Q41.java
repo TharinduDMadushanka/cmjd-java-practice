7d2e52f4ff9d0f7a29a5fed16c366dd9d5bba563
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-24 18:31:55 +0530

    first commit

--- src/Day12_OOP4/Q41.java
+++ src/Day12_OOP4/Q41.java
@@ -3,5 +3,14 @@
  */
 
 package Day12_OOP4;
-public class Q41 {
+/*abstract class Vehicle{
+    abstract public void park();
+    abstract public void start();
+    abstract public void stop();
+}*/
+
+interface Vehicle1{
+    public void park();
+    public void start();
+    public void stop();
 }
