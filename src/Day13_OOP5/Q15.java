4eb2ec321816fc6076700d94301a708ac1de8d0a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-25 10:58:00 +0530

    first commit

--- src/Day13_OOP5/Q15.java
+++ src/Day13_OOP5/Q15.java
@@ -6,47 +6,47 @@ package Day13_OOP5;
  * create class before it used
  * create inner cls in a method body
  */
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-
-class Calculator extends JFrame{
-
-    JButton button;
-    JTextField textField;
-
-    Calculator(){
-        setSize(300,300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocationRelativeTo(null);
-        setLayout(new FlowLayout());
-
-        textField = new JTextField(10);
-        textField.setFont(new Font("", 1, 25));
-        add(textField);
-
-        button = new JButton();
-        button.setText("Button");
-        button.setFont(new Font("", 1, 25));
-
-        /////////////////////////////
-        class MyClass implements ActionListener{
-            public void actionPerformed(ActionEvent e){
-                String title = textField.getText();
-                setTitle(title);
-            }
-        }
-        /////////////////////////////
-
-        button.addActionListener(new MyClass());
-
-        add(button);
-        setVisible(true);
-    }
-}
-
-class Example {
-    public static void main(String[] args) {
-        new Calculator();
-    }
-}
+//import java.awt.*;
+//import java.awt.event.*;
+//import javax.swing.*;
+//
+//class Calculator extends JFrame{
+//
+//    JButton button;
+//    JTextField textField;
+//
+//    Calculator(){
+//        setSize(300,300);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setLocationRelativeTo(null);
+//        setLayout(new FlowLayout());
+//
+//        textField = new JTextField(10);
+//        textField.setFont(new Font("", 1, 25));
+//        add(textField);
+//
+//        button = new JButton();
+//        button.setText("Button");
+//        button.setFont(new Font("", 1, 25));
+//
+//        /////////////////////////////
+//        class MyClass implements ActionListener{
+//            public void actionPerformed(ActionEvent e){
+//                String title = textField.getText();
+//                setTitle(title);
+//            }
+//        }
+//        /////////////////////////////
+//
+//        button.addActionListener(new MyClass());
+//
+//        add(button);
+//        setVisible(true);
+//    }
+//}
+//
+//class Example {
+//    public static void main(String[] args) {
+//        new Calculator();
+//    }
+//}
