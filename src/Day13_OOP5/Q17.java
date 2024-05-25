db7a0031c6ef9473b82ab88320538acb4f5eb75c
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-25 11:52:39 +0530

    first commit

--- src/Day13_OOP5/Q17.java
+++ src/Day13_OOP5/Q17.java
@@ -3,19 +3,19 @@ package Day13_OOP5;
 /**
  * We cant create interface obj type bt we can override interface class method nd access
  */
-interface Vehicle{
-    void park();
-}
-
-class Q17 {
-    public static void main(String[] args) {
-        Vehicle v1 = new Vehicle() {
-            public void park(){
-                System.out.println("Car Parking");
-            }
-        };
-
-        v1.park();
-    }
-}
+//interface Vehicle{
+//    void park();
+//}
+//
+//class Q17 {
+//    public static void main(String[] args) {
+//        Vehicle v1 = new Vehicle() {
+//            public void park(){
+//                System.out.println("Car Parking");
+//            }
+//        };
+//
+//        v1.park();
+//    }
+//}
 
