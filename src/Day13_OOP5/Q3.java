7792ba1fa771ba869dce6b5f4605112b8ee3d70f
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-25 08:57:39 +0530

    first commit

---
+++ src/Day13_OOP5/Q3.java
@@ -0,0 +1,9 @@
+/**
+ * Case 12
+ * interface - interface ---> extends (we can do multiple extends with 2 interfaces)
+ * interface - class ---> implements
+ *
+ */
+package Day13_OOP5;
+public class Q3 {
+}
