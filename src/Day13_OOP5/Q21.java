80d9a656ba27038d87747ad6f6f449017f78567b
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-25 12:33:00 +0530

    first commit

--- src/Day13_OOP5/Q21.java
+++ src/Day13_OOP5/Q21.java
@@ -3,24 +3,24 @@
  * (Since JDK 1.8)
  */
 package Day13_OOP5;
-interface Vehicle{
-    void park();
-}
-
-class Example {
-    public static void main(String[] args) {
-        Vehicle v1 = new Vehicle(){
-            public void park() {
-                System.out.println("Car Parking");
-            }
-        };
-
-        v1.park();
-
-        Vehicle v2 = () -> {
-            System.out.println("Car Parking... Lambda Expression");
-        };
-        v2.park();
-    }
-}
+//interface Vehicle{
+//    void park();
+//}
+//
+//class Example {
+//    public static void main(String[] args) {
+//        Vehicle v1 = new Vehicle(){
+//            public void park() {
+//                System.out.println("Car Parking");
+//            }
+//        };
+//
+//        v1.park();
+//
+//        Vehicle v2 = () -> {
+//            System.out.println("Car Parking... Lambda Expression");
+//        };
+//        v2.park();
+//    }
+//}
 
