e5148c818a295ed0aaef9b5575a4968c65c91e8a
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-27 09:28:05 +0530

    first commit

--- src/Day13_OOP5/Q26.java
+++ src/Day13_OOP5/Q26.java
@@ -3,5 +3,20 @@ package Day13_OOP5;
 /**
  * Case 5
  */
-public class Q26 {
+interface Vehicle{
+    boolean park();
+}
+
+class Example {
+    public static void main(String[] args) {
+        Vehicle v1 = ()-> true;
+
+        //Vehicle v2 = ()-> return true; // Illegal
+
+        Vehicle v2 = ()-> {
+            //
+            //
+            return true;
+        };
+    }
 }
