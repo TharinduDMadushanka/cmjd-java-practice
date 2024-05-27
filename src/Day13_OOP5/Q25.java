c5b118396c77bd517d268c74733cc148ee6a82d2
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-27 09:27:03 +0530

    first commit

--- src/Day13_OOP5/Q25.java
+++ src/Day13_OOP5/Q25.java
@@ -2,6 +2,23 @@ package Day13_OOP5;
 
 /**
  * Case 4
+ * x --> location , no need to re passing again
  */
-public class Q25 {
+interface Vehicle{
+    void park(int location);
+}
+
+class Example {
+    public static void main(String[] args) {
+        Vehicle v1 = (x) -> {
+            System.out.println("Car parking..." + x);
+        };
+        v1.park(1000);
+
+        Vehicle v2 = (int location) -> {
+            System.out.println("Car parking..." + location);
+        };
+
+        v2.park(100);
+    }
 }
