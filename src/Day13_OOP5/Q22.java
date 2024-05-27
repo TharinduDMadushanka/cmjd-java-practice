f57bc5b8754dd052fcf3bc962f67e672c23ccac9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-05-27 09:23:17 +0530

    first commit

--- src/Day13_OOP5/Q22.java
+++ src/Day13_OOP5/Q22.java
@@ -3,5 +3,14 @@ package Day13_OOP5;
 /**
  * Case 1
  */
-public class Q22 {
+interface Vehicle{
+    void park();
 }
+
+class Q22 {
+    public static void main(String[] args) {
+        Vehicle v1 = () -> System.out.println("Car parking... Lambda"); // one statements no need code block
+        v1.park();
+    }
+}
+
