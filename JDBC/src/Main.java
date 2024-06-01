89685712cbb2288a0be0eef8b48b150a4dadc75e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 09:36:35 +0530

    first commit

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -6,8 +6,17 @@ public class Main {
 
         Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket","root","Thariya920@");
 
+        // Step 03
         Statement statement = connection.createStatement();
         String sql= "SELECT * FROM customer";
         ResultSet resultSet= statement.executeQuery(sql);
+
+        // Step 04 --> Execute query
+        while (resultSet.next()){
+            System.out.println(resultSet.getString("CustID" + " " +
+                    resultSet.getString("CustTitle") +
+                    " " + resultSet.getString("CustName") +
+                    " " + resultSet.getDouble("salary")));
+        }
     }
 }
