3103fc7c0f9fc43b8726ca8853ddc3eecc051ba5
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 12:34:15 +0530

    first commit

--- JDBC/src/DBConnection.java
+++ JDBC/src/DBConnection.java
@@ -13,7 +13,7 @@ public class DBConnection {
 
     private DBConnection() throws ClassNotFoundException, SQLException{
         Class.forName("com.mysql.cj.jdbc.Driver");
-        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "mysql");
+        connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "Thariya920@");
     }
 
     public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
