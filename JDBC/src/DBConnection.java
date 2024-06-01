43a4696c2afe7ccc9b63977a9b3096a65933d6d4
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 12:25:33 +0530

    first commit

---
+++ JDBC/src/DBConnection.java
@@ -0,0 +1,16 @@
+public class DBConnection {
+
+    private static DBConnection dbConnection;
+
+    private DBConnection() {
+
+    }
+
+    public static DBConnection getInstance() {
+        if (dbConnection == null) {
+            dbConnection = new DBConnection();
+        }
+        return dbConnection;
+    }
+
+}
