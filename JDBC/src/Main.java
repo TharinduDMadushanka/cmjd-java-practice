c1bfe813ca7b73fc19795f2f605351a08ac51f16
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 09:32:17 +0530

    first commit

--- JDBC/src/Main.java
+++ JDBC/src/Main.java
@@ -1,11 +1,13 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 
 public class Main {
     public static void main(String[] args) throws ClassNotFoundException, SQLException { // Step 02
         Class.forName("com.mysql.cj.jdbc.Driver"); // Step 01 --> load mysql driver
 
         Connection connection= DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket","root","Thariya920@");
+
+        Statement statement = connection.createStatement();
+        String sql= "SELECT * FROM customer";
+        ResultSet resultSet= statement.executeQuery(sql);
     }
 }
