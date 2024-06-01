66f9effa2f72e987a4b5b06d28e25ef552d51365
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-01 10:21:38 +0530

    first commit

--- JDBC/src/Q2.java
+++ JDBC/src/Q2.java
@@ -13,44 +13,42 @@ public class Q2 {
 
         Scanner scanner = new Scanner(System.in);
 
-        System.out.print("Input Customer ID:");
-        String custId = scanner.next();
+        System.out.print("Input Customer Id : ");
+        String custId = scanner.nextLine();
 
-        System.out.print("Input Customer Title:");
-        String custTitle = scanner.next();
+        System.out.print("Input Customer Title : ");
+        String title = scanner.nextLine();
 
-        System.out.print("Input Customer Name:");
-        String custName = scanner.next();
+        System.out.print("Input Customer Name : ");
+        String name = scanner.nextLine();
 
-        System.out.print("Input Customer DoB:");
-        String custDob = scanner.next();
+        System.out.print("Input Customer DOB : ");
+        String dob = scanner.nextLine();
 
-        System.out.print("Input Customer Salary:");
-        double salary  = scanner.nextDouble();
+        System.out.print("Input Customer Address : ");
+        String address = scanner.nextLine();
 
-        System.out.print("Input Customer Address:");
-        String custAddress = scanner.next();
+        System.out.print("Input Customer City : ");
+        String city = scanner.nextLine();
 
-        System.out.print("Input Customer City:");
-        String custCity = scanner.next();
+        System.out.print("Input Customer Province : ");
+        String province = scanner.nextLine();
 
-        System.out.print("Input Customer Province:");
-        String custProvince = scanner.next();
+        System.out.print("Input Customer Zip : ");
+        String zip = scanner.nextLine();
 
-        System.out.print("Input Customer Zip code:");
-        String zip = scanner.next();
+        System.out.print("Input Customer Salary : ");
+        double salary = scanner.nextDouble();
 
-        Class.forName("com.mysql.cj.jdbc.Driver"); // Step 01 --> load mysql driver
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/Supermarket", "root", "mysql");
 
-        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/supermarket", "root", "Thariya920@");
-
-        // Step 03
         Statement statement = connection.createStatement();
-
-        String sql ="INSERT INTO Customer VALUES ('"+custId+"'"+custTitle+"'"+custName+"'"+custDob+"'"+salary+"'"+custAddress+"'"+custCity+"'"+custProvince+"'"+zip;
+        String sql = "INSERT INTO Customer VALUES ('"+custId+"', '"+title+"', '"+name+"', '"+dob+"', "+salary+", '"+address+"', '"+city+"', '"+province+"', '"+zip+"')";
 
         int result = statement.executeUpdate(sql);
 
         System.out.println(result > 0 ? "Success" : "Fail");
+
     }
 }
