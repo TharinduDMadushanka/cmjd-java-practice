0e571bb5ee9ab80dd39b820d903f73b5b0c2cb1e
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-22 10:03:38 +0530

    cmjd 06/22 lec

--- src/June22/Q13.java
+++ src/June22/Q13.java
@@ -1,7 +1,7 @@
 package June22;
 
 /**
- * Case 01
+ * Case 02
  */
 
 //class A{
@@ -18,15 +18,20 @@ package June22;
 //    }
 //}
 //
-//class Example12 {
+//class Example13 {
 //    public static void main(String args[]) {
 //        A a1 = new A();
 //        a1.a = 100;
 //        a1.mA();
 //
-//        B b1 = (B) a1; // Legal, Runtime Error
-//        b1.b = 200;
-//        b1.mB();
-//    }
+//        if (a1 instanceof B){
+//            B b1 = (B) a1;
+//            b1.b = 200;
+//            b1.mB();
+//        }
 //
+//        System.out.println("a1 instanceof B : " + (a1 instanceof B));
+//
+//    }
 //}
+
