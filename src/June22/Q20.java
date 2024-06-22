b076af933aeb4d50da1fd691eebe12916b8ac2e9
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-22 10:52:06 +0530

    cmjd 06/22 lec

---
+++ src/June22/Q20.java
@@ -0,0 +1,12 @@
+package June22;
+
+enum Color1{
+    RED, GREEN, BLACK, BLUE;// Enum ObjectsS
+}
+
+class Example20 {
+    public static void main(String args[]) {
+        //Color c1 = new Color(); // Error
+        Color1 c1 = Color1.RED; // Legal
+    }
+}
