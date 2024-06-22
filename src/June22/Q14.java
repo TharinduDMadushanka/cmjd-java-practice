29cbf7e292e93dfa4de2b7fa45654b896ca7d2db
TharinduDMadushanka <tharindudmadushanka920@gmail.com>
2024-06-22 10:09:16 +0530

    cmjd 06/22 lec

--- src/June22/Q14.java
+++ src/June22/Q14.java
@@ -19,31 +19,30 @@ class Example14 {
 
         C c1 = new C();
 
-        System.out.println("a1 instanceof A " + (a1 instanceof A));
-        System.out.println("a1 instanceof B " + (a1 instanceof B));
-        System.out.println("a1 instanceof C " + (a1 instanceof C));
+        System.out.println("a1 instanceof A : " + (a1 instanceof A)); // true
+        System.out.println("a1 instanceof B : " + (a1 instanceof B)); // false
+        System.out.println("a1 instanceof C : " + (a1 instanceof C)); // false
 
-        System.out.println("a2 instanceof A " + (a2 instanceof A));
-        System.out.println("a2 instanceof B " + (a2 instanceof B));
-        System.out.println("a2 instanceof C " + (a2 instanceof C));
+        System.out.println("a2 instanceof A : " + (a2 instanceof A)); // true
+        System.out.println("a2 instanceof B : " + (a2 instanceof B)); // true
+        System.out.println("a2 instanceof C : " + (a2 instanceof C)); // false
 
-        System.out.println("a3 instanceof A " + (a3 instanceof A));
-        System.out.println("a3 instanceof B " + (a3 instanceof B));
-        System.out.println("a3 instanceof C " + (a3 instanceof C));
+        System.out.println("a3 instanceof A : " + (a3 instanceof A)); // true
+        System.out.println("a3 instanceof B : " + (a3 instanceof B)); // true
+        System.out.println("a3 instanceof C : " + (a3 instanceof C)); // true
 
-        System.out.println("b1 instanceof A " + (b1 instanceof A));
-        System.out.println("b1 instanceof B " + (b1 instanceof B));
-        System.out.println("b1 instanceof C " + (b1 instanceof C));
+        System.out.println("b1 instanceof A : " + (b1 instanceof A)); // true
+        System.out.println("b1 instanceof B : " + (b1 instanceof B)); // true
+        System.out.println("b1 instanceof C : " + (b1 instanceof C)); // false
 
-        System.out.println("b2 instanceof A " + (b2 instanceof A));
-        System.out.println("b2 instanceof B " + (b2 instanceof B));
-        System.out.println("b2 instanceof C " + (b2 instanceof C));
+        System.out.println("b2 instanceof A : " + (b2 instanceof A)); // true
+        System.out.println("b2 instanceof B : " + (b2 instanceof B)); // true
+        System.out.println("b2 instanceof C : " + (b2 instanceof C)); // true
 
-        System.out.println("c1 instanceof A " + (c1 instanceof A));
-        System.out.println("c1 instanceof B " + (c1 instanceof B));
-        System.out.println("c1 instanceof C " + (c1 instanceof C));
-
-
+        System.out.println("c1 instanceof A : " + (c1 instanceof A)); // true
+        System.out.println("c1 instanceof B : " + (c1 instanceof B)); // true
+        System.out.println("c1 instanceof C : " + (c1 instanceof C)); // true
 
     }
 }
+
